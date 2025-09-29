c13a5d2838e679ff5ac1da69616f44ccd9e7eb05
Nisaur Rohmah <113544206+saurday@users.noreply.github.com>
2025-09-29 11:21:16 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/run-dosen-seeder', function () {
+    Artisan::call('db:seed', [
+        '--class' => 'DosenSeeder',
+        '--force' => true,
+    ]);
+    return "DosenSeeder executed!";
+});
